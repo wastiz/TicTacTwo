bda07d1e13d7ef225070da4541387976617567dd
wastiz <80074753+wastiz@users.noreply.github.com>
2025-01-18 19:59:27 +0200

    another

--- Tic-Tac-Two Console/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ Tic-Tac-Two Console/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b80e0b04662c76d36ea0122536c60b28acad5c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
