e9ee2f644c37041836dfcaa949ec7fc5483ca266
wastiz <valnos04@gmail.com>
2025-01-22 00:28:17 +0200

    another

--- Tic-Tac-Two Web/DAL/AppDbContext.cs
+++ Tic-Tac-Two Web/DAL/AppDbContext.cs
@@ -11,21 +11,9 @@ namespace DAL
 
         public AppDbContext(DbContextOptions<AppDbContext>? options = null)
             : base(options ?? new DbContextOptionsBuilder<AppDbContext>()
-                .UseNpgsql(GetConnectionString())
+                .UseNpgsql($"Host=localhost;Database=tic_tac_two;Username=postgres;Password=admin")
                 .Options)
         {
-        }
-
-        private static string GetConnectionString()
-        {
-            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-
-            return environment switch
-            {
-                "Development" => "Host=localhost;Database=tic_tac_two;Username=postgres;Password=admin",
-                "Production" => "Host=localhost;Database=tic_tac_two;Username=postgres;Password=PGtitan4321",
-                _ => throw new InvalidOperationException($"Unknown environment: {environment}")
-            };
         }
     }
 }
